d4c21aea321490408abecf5bcc21eb33c51d5eea
John Kleinschmidt <jkleinsc@electronjs.org>
2021-01-20 18:58:53 -0500

    codesigning doesn't work on m1 macs

--- spec-main/api-autoupdater-darwin-spec.ts
+++ spec-main/api-autoupdater-darwin-spec.ts
@@ -13,7 +13,7 @@ const features = process._linkedBinding('electron_common_features');
 const fixturesPath = path.resolve(__dirname, 'fixtures');
 
 // We can only test the auto updater on darwin non-component builds
-ifdescribe(process.platform === 'darwin' && !process.mas && !features.isComponentBuild())('autoUpdater behavior', function () {
+ifdescribe(process.platform === 'darwin' && process.arch !== 'arm64' && !process.mas && !features.isComponentBuild())('autoUpdater behavior', function () {
   this.timeout(120000);
 
   let identity = '';
